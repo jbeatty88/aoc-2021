3ff099210963f7200f0e5b87b2fe02598902af9e
joshbeatty <jbeatty@intraprisehealth.com>
2021-12-02 09:54:44 -0700

    Add code for day02 solution

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -3,20 +3,54 @@ package day02
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+  fun part1(input: List<String>): Int {
+    var y = 0
+    var x = 0
+    input.forEach {
+      val (direction, value) = it.split(" ")
+      when (direction) {
+        "down" -> {
+          y += value.toInt()
+        }
+        "up" -> {
+          y -= value.toInt()
+        }
+        else -> {
+          x += value.toInt()
+        }
+      }
     }
+    return x * y
+  }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+  fun part2(input: List<String>): Int {
+    var y = 0
+    var x = 0
+    var z = 0
+    input.forEach {
+      val (direction, value) = it.split(" ")
+      when (direction) {
+        "down" -> {
+          z += value.toInt()
+        }
+        "up" -> {
+          z -= value.toInt()
+        }
+        else -> {
+          x += value.toInt()
+          y += z * value.toInt()
+        }
+      }
     }
+    return x * y
+  }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("day02/Day02_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+  // test if implementation meets criteria from the description, like:
+  val testInput = readInput("day02/Day02_test")
+  check(part1(testInput) == 150)
+  check(part2(testInput) == 900)
 
-    val input = readInput("day02/day02")
-    println(part1(input))
-    println(part2(input))
+  val input = readInput("day02/day02")
+  println(part1(input))
+  println(part2(input))
 }
