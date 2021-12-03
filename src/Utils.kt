55846ede346511a45f30264e9c9127d964e1f13f
joshbeatty <jbeatty@intraprisehealth.com>
2021-12-03 15:53:35 -0700

    Add code for day03 solution

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,52 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+/**
+ * Takes a list of Strings of equal length and return vertical strings by character index mapped my index
+ *
+ * Example:
+ *  ["Hello", "World"] -> ["HW", "eo", "lr", "ll", "od"]
+ *
+ * @param input List of strings of equal length
+ * @return a vertical strings mapped to index
+ */
+fun toVerticalStringMapByIdx(input: List<String>): MutableMap<Int, String> {
+    val verticalStrings = mutableMapOf<Int, String>()
+
+    input.forEach {
+        // Get the vertical binary strings for gamma and epsilon calculations
+        for (i in it.indices) {
+            verticalStrings[i] =
+                if (verticalStrings[i].isNullOrBlank()) it[i].toString() else verticalStrings[i] + it[i]
+        }
+    }
+    return verticalStrings
+}
+
+fun mutableListToVerticalStringMapByIdx(input: MutableList<String>): MutableMap<Int, String> {
+    val verticalStrings = mutableMapOf<Int, String>()
+
+    input.forEach {
+        // Get the vertical binary strings for gamma and epsilon calculations
+        for (i in it.indices) {
+            verticalStrings[i] =
+                if (verticalStrings[i].isNullOrBlank()) it[i].toString() else verticalStrings[i] + it[i]
+        }
+    }
+    return verticalStrings
+}
+
+fun mapFrequenciesToIdx(verticalStrings: MutableMap<Int, String>, idxRange: Int): MutableMap<Int, MutableMap<Char, Int>> {
+    val freqs: MutableMap<Int, MutableMap<Char, Int>> = HashMap()
+    for (i in 0 until idxRange) {
+        for (c in verticalStrings[i].toString()) {
+            freqs.putIfAbsent(i, mutableMapOf(c to 0))
+            freqs[i]?.putIfAbsent(c, 0)
+            freqs[i]?.putIfAbsent('0', 0)
+            freqs[i]?.putIfAbsent('1', 0)
+            freqs[i]?.set(c, freqs[i]?.get(c)!! + 1)
+        }
+    }
+    return freqs
+}
--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -1,20 +1,71 @@
 package day03
 
+import mapFrequenciesToIdx
+import mutableListToVerticalStringMapByIdx
 import readInput
+import toVerticalStringMapByIdx
 
 fun main() {
   fun part1(input: List<String>): Int {
-    return input.size
+    val verticalStrings = toVerticalStringMapByIdx(input)
+
+    val freqs = mapFrequenciesToIdx(verticalStrings, input[0].length)
+
+    // Calculate the gamma and epsilon values
+    val gammaEpsilon: MutableMap<String, String> = HashMap()
+    for (i in 0 until input[0].length) {
+      gammaEpsilon.putIfAbsent("gamma", "")
+      gammaEpsilon.putIfAbsent("epsilon", "")
+      if (freqs[i]?.get('0')!! > freqs[i]?.get('1')!!) {
+        gammaEpsilon["gamma"] = gammaEpsilon["gamma"] + "0"
+        gammaEpsilon["epsilon"] = gammaEpsilon["epsilon"] + "1"
+      } else {
+        gammaEpsilon["gamma"] = gammaEpsilon["gamma"] + "1"
+        gammaEpsilon["epsilon"] = gammaEpsilon["epsilon"] + "0"
+      }
+    }
+
+    val gammaDec = Integer.parseInt(gammaEpsilon["gamma"], 2)
+    val epsilonDec = Integer.parseInt(gammaEpsilon["epsilon"], 2)
+
+    return gammaDec * epsilonDec
   }
 
   fun part2(input: List<String>): Int {
-    return input.size
+    var filteredO2Input = input.toMutableList()
+    var filteredCO2Input = input.toMutableList()
+
+    var o2VerticalStrings = mutableListToVerticalStringMapByIdx(filteredO2Input)
+    var co2VerticalStrings = mutableListToVerticalStringMapByIdx(filteredCO2Input)
+
+    var o2Freqs = mapFrequenciesToIdx(o2VerticalStrings, input[0].length)
+    var co2Freqs = mapFrequenciesToIdx(co2VerticalStrings, input[0].length)
+
+      for (i in 0 until o2Freqs.size) {
+        if (filteredO2Input.size == 1) continue
+        val o2Criterion = if (o2Freqs[i]?.get('1')!! >= o2Freqs[i]?.get('0')!!) '1' else '0'
+        filteredO2Input = filteredO2Input.filter { it[i] == o2Criterion}.toMutableList()
+        o2VerticalStrings = mutableListToVerticalStringMapByIdx(filteredO2Input)
+        o2Freqs = mapFrequenciesToIdx(o2VerticalStrings, input[0].length)
+      }
+
+
+      for (i in 0 until co2Freqs.size) {
+        if (filteredCO2Input.size == 1) continue
+        val co2Criterion = if (co2Freqs[i]?.get('0')!! <= co2Freqs[i]?.get('1')!!) '0' else '1'
+        filteredCO2Input = filteredCO2Input.filter { it[i] == co2Criterion}.toMutableList()
+        co2VerticalStrings = mutableListToVerticalStringMapByIdx(filteredCO2Input)
+        co2Freqs = mapFrequenciesToIdx(co2VerticalStrings, input[0].length)
+      }
+
+
+    return Integer.parseInt(filteredO2Input[0], 2) * Integer.parseInt(filteredCO2Input[0], 2)
   }
 
   // test if implementation meets criteria from the description, like:
   val testInput = readInput("day03/Day03_test")
-  check(part1(testInput) == 0)
-  check(part2(testInput) == 0)
+  check(part1(testInput) == 198)
+  check(part2(testInput) == 230)
 
   val input = readInput("day03/day03")
   println(part1(input))
