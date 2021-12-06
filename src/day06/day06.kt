da794b3f8e366815b74265326c3f82e4548d5f31
joshbeatty <jbeatty@intraprisehealth.com>
2021-12-05 23:58:30 -0700

    Setup for day05 and day06

---
+++ src/day06/day06.kt
@@ -0,0 +1,22 @@
+package day06
+
+import readInput
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("day05/Day05_test")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    val input = readInput("day05/day05")
+    println("Part 1 Answer: ${part1(input)}")
+    println("Part 2 Answer: ${part2(input)}")
+}
