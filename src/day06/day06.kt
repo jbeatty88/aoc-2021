b241bf5a703367753294ca4a04ded247edd2404e
joshbeatty <jbeatty@intraprisehealth.com>
2021-12-11 00:14:57 -0700

    Add code for solution to day06 part1 only

--- src/day06/day06.kt
+++ src/day06/day06.kt
@@ -4,19 +4,45 @@ import readInput
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        var lanternFishes = input[0].split(",").map { it.toInt() }.toMutableList()
+        println("Initial State $lanternFishes")
+        for (i in 1..80) {
+            var lanternFishSnapshot: List<Int> = ArrayList(lanternFishes)
+            lanternFishSnapshot.forEachIndexed {fishIdx, fishTimer ->
+                if (fishTimer == 0) {
+                    lanternFishes.add(8)
+                    lanternFishes[fishIdx] = 6
+                } else {
+                    lanternFishes[fishIdx]--
+                }
+            }
+        }
+        return lanternFishes.size
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        var lanternFishes = input[0].split(",").map { it.toInt() }.toMutableList()
+        println("Initial State $lanternFishes")
+        for (i in 1..256) {
+            var lanternFishSnapshot: List<Int> = ArrayList(lanternFishes)
+            lanternFishSnapshot.forEachIndexed {fishIdx, fishTimer ->
+                if (fishTimer == 0) {
+                    lanternFishes.add(8)
+                    lanternFishes[fishIdx] = 6
+                } else {
+                    lanternFishes[fishIdx]--
+                }
+            }
+        }
+        return lanternFishes.size.toLong()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("day05/Day05_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    val testInput = readInput("day06/Day06_test")
+    check(part1(testInput) == 5934)
+//    check(part2(testInput) == 26984457539)
 
-    val input = readInput("day05/day05")
+    val input = readInput("day06/day06")
     println("Part 1 Answer: ${part1(input)}")
-    println("Part 2 Answer: ${part2(input)}")
+//    println("Part 2 Answer: ${part2(input)}")
 }
